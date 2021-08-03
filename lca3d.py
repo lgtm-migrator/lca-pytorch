85fbb6ce38687704f0ee49b5dd91981124fb379c
Michael Teti <mtetifau@gmail.com>
2021-08-03 12:12:41 -0400

    everything working now

--- lca3d.py
+++ lca3d.py
@@ -1,8 +1,9 @@
 import os 
-from random import shuffle
+from random import randint, shuffle
 
 import cv2
 import h5py
+import imageio
 import matplotlib.pyplot as plt 
 import numpy as np
 from progressbar import ProgressBar
@@ -15,7 +16,8 @@ from torchvision.utils import make_grid
 
 BATCH_SIZE = 128
 DEVICE = 1
-DTYPE = torch.float16
+DTYPE = torch.float32
+N_FRAMES_IN_TIME = 5
 UPDATE_STEPS = 1500
 
 
@@ -23,7 +25,7 @@ class LCADLConvSpatialComp:
     def __init__(self, n_neurons, in_c, thresh = 0.1, tau = 1500, 
                  eta = 0.01, lca_iters = 2000, kh = 7, kw = 7, kt = 9, stride_h = 1, stride_w = 1, 
                  stride_t = 1, pad = 'same', device = None, dtype = torch.float32, learn_dict = True,
-                 nonneg = True, track_loss = True, dict_write_step = -1, 
+                 nonneg = True, track_metrics = True, dict_write_step = -1, 
                  act_write_step = -1, input_write_step = -1, recon_write_step = -1, 
                  result_fpath = 'LCA_results', scale_imgs = True, zero_center_imgs = True):
         ''' Performs sparse dictionary learning via LCA (Rozell et al. 2008) '''
@@ -46,7 +48,7 @@ class LCADLConvSpatialComp:
         self.dtype = dtype
         self.learn_dict = learn_dict
         self.nonneg = nonneg
-        self.track_loss = track_loss
+        self.track_metrics = track_metrics
         self.dict_write_step = dict_write_step 
         self.act_write_step = act_write_step
         self.input_write_step = input_write_step
@@ -59,7 +61,7 @@ class LCADLConvSpatialComp:
         assert(self.kh % 2 != 0 and self.kw % 2 != 0)
 
         # We will store l2 reconstruction values in these to plot 
-        if self.track_loss:
+        if self.track_metrics:
             self.recon_error_mean, self.recon_error_se = [], []
             self.l1_sparsity_mean, self.l1_sparsity_se = [], []
 
@@ -81,7 +83,7 @@ class LCADLConvSpatialComp:
 
     def compute_padding_dims(self):
         ''' Computes padding for forward and transpose convs '''
-        self.input_pad = (0, (self.kh-1)//2, (self.kw-1)//2) if self.pad == 'same' else 0
+        self.input_pad = (0, (self.kh-1)//2, (self.kw-1)//2) if self.pad == 'same' else (0, 0, 0)
         self.recon_output_pad = (
             0,
             self.stride_h - 1 if (self.kh % 2 != 0 and self.stride_h > 1) else 0, 
@@ -97,7 +99,7 @@ class LCADLConvSpatialComp:
         )
         # to avoid inhibition from future neurons to past neurons
         # if kt != input depth
-        G[:, :, (G.shape[2]-1)//2+1:, :, :] = 0.0
+        # G[:, :, (G.shape[2]-1)//2+1:, :, :] = 0.0
         if not hasattr(self, 'n_surround_h'):
             self.n_surround_t = int(np.ceil((G.shape[-3] - 1) / 2))
             self.n_surround_h = int(np.ceil((G.shape[-2] - 1) / 2))
@@ -140,10 +142,11 @@ class LCADLConvSpatialComp:
             u_t += self.charge_rate * (b_t - u_t - self.lateral_competition(a_t, G) + a_t)
 
 
-        if self.track_loss: self.track_l1_sparsity(a_t)
+        if self.track_metrics: self.track_l1_sparsity(a_t)
         if self.ts % self.act_write_step == 0 and self.act_write_step != -1:
             self.append_h5(self.result_fpath, 'a_{}'.format(self.ts), a_t)
 
+        assert not any(torch.isnan(a_t).flatten())
         return a_t 
 
     def update_D(self, x, a):
@@ -155,7 +158,7 @@ class LCADLConvSpatialComp:
             self.append_h5(self.result_fpath, 'recon_{}'.format(self.ts), recon)
 
         error = x - recon
-        if self.track_loss: self.track_l2_recon_error(error)
+        if self.track_metrics: self.track_l2_recon_error(error)
         error = F.pad(
             error,
             (
@@ -189,8 +192,10 @@ class LCADLConvSpatialComp:
     def normalize_D(self, eps = 1e-12):
         ''' Normalizes features such at each one has unit norm '''
 
-        scale = (self.D.norm(p = 2, dim = (1, 2, 3), keepdim = True) + eps)
+        scale = (self.D.norm(p = 2, dim = (1, 2, 3, 4), keepdim = True) + eps)
         self.D /= scale
+        assert not any(torch.isnan(self.D).flatten())
+        assert all(self.D.norm(p=2, dim = (1,2,3,4)) >= 0.99)
 
     def soft_threshold(self, x):
         ''' Soft threshold '''
@@ -211,12 +216,12 @@ class LCADLConvSpatialComp:
 
     def track_l2_recon_error(self, error):
         ''' Keeps track of the reconstruction error over training '''
-        l2_error = error.norm(p = 2, dim = (1, 2, 3))
+        l2_error = error.norm(p = 2, dim = (1, 2, 3, 4))
         self.recon_error_mean.append(torch.mean(l2_error).item())
         self.recon_error_se.append(torch.std(l2_error).item() / np.sqrt(error.shape[0]))
 
     def track_l1_sparsity(self, acts):
-        l1_sparsity = acts.norm(p = 1, dim = (1, 2, 3))
+        l1_sparsity = acts.norm(p = 1, dim = (1, 2, 3, 4))
         self.l1_sparsity_mean.append(torch.mean(l1_sparsity).item())
         self.l1_sparsity_se.append(torch.std(l1_sparsity).item() / np.sqrt(acts.shape[0]))
 
@@ -259,33 +264,39 @@ class LCADLConvSpatialComp:
 
 
 # loading in random images from CIFAR dataset
-imgs = CIFAR10(root = 'cifar/', download = True).data.astype(np.float32)
-imgs_gray = torch.from_numpy(np.mean(imgs, -1)).unsqueeze(1)
-imgs_gray = torch.stack([imgs_gray for _ in range(9)], dim = 2)
-train, test = imgs_gray[:-100], imgs_gray[-100:]
+data_dir = '/media/mteti/1TB_SSD/NEMO/data/ILSVRC2015/Data/VID/train/ILSVRC2015_VID_train_0000/'
+vid_dirs = [os.path.join(data_dir, d) for d in os.listdir(data_dir) if 'resized' in d]
+fpaths = [[os.path.join(d, f) for f in os.listdir(d)] for d in vid_dirs]
 
 
 # run the model
 model = LCADLConvSpatialComp(
-    64, 
+    128, 
     1,
-    lca_iters = 1500, 
-    thresh = 0.1, 
+    lca_iters = 1000, 
+    thresh = 0.125, 
     device = DEVICE,
-    stride_h = 2,
-    stride_w = 2,
+    stride_h = 4,
+    stride_w = 4,
     stride_t = 1,
-    kh = 9,
-    kw = 9,
-    kt = 9,
+    kh = 13,
+    kw = 13,
+    kt = N_FRAMES_IN_TIME,
     nonneg = False,
     pad = 'same',
     dtype = DTYPE,
-    tau = 1000
+    tau = 500,
+    eta = 1e-3
 )
 
 for step in ProgressBar()(range(UPDATE_STEPS)):
-    batch = train[np.random.choice(train.shape[0], BATCH_SIZE, replace = False)]
+    batch_vids = [fpaths[ind] for ind in np.random.choice(len(fpaths), BATCH_SIZE, replace = False)]
+    frame_inds = [randint(0, len(vid)-N_FRAMES_IN_TIME) if len(vid) >= N_FRAMES_IN_TIME else 'remove' for vid in batch_vids]
+    batch_frames = [v[ind:ind+N_FRAMES_IN_TIME] for v, ind in zip(batch_vids, frame_inds) if ind != 'remove']
+    batch_imgs = [[torch.from_numpy(cv2.imread(img_fpath, cv2.IMREAD_GRAYSCALE)) for img_fpath in f] for f in batch_frames]
+    batch_imgs_tensor = [torch.unsqueeze(torch.stack(imgs), dim = 0) for imgs in batch_imgs]
+    batch = torch.stack(batch_imgs_tensor)
+    batch = batch[..., 16:48]
     batch = batch.type(DTYPE).to(DEVICE)
     a = model.forward(batch)
 
@@ -311,26 +322,18 @@ plt.xlabel('Training Iteration')
 plt.show()
 
 # plot dictionary
-grid = make_grid(model.D, nrow = int(np.sqrt(model.m)))
-plt.imshow(grid.float().cpu().numpy()[0], cmap = 'gray')
-plt.show()
-
+grids = [make_grid(model.D[:, :, t], nrow = int(np.sqrt(model.m))) for t in range(model.D.shape[2])]
+imageio.mimwrite('lca3d_feats.gif', [grid[0].float().cpu().numpy() for grid in grids], fps = 5)
 
 # reconstruct new images
-inputs = model.preprocess_inputs(test)
-inputs = inputs.type(DTYPE).to(DEVICE)
-a = model.encode(inputs)
+batch = model.preprocess_inputs(batch)
+a = model.encode(batch)
 recon = model.compute_recon(a) 
 
-
-# plot inputs and recons
-fig = plt.figure()
-sub1 = fig.add_subplot(121)
-sub2 = fig.add_subplot(122)
-in_grid = make_grid(inputs, nrow = 10)
-rec_grid = make_grid(recon, nrow = 10)
-sub1.imshow(in_grid.float().cpu().numpy()[0], cmap = 'gray')
-sub2.imshow(rec_grid.float().cpu().numpy()[0], cmap = 'gray')
-sub1.set_title('Input')
-sub2.set_title('Recon')
-plt.show()
+# write out input and recon .gifs
+input_grids = [make_grid(batch[:, :, t], nrow = int(np.sqrt(BATCH_SIZE))) for t in range(N_FRAMES_IN_TIME)]
+recon_grids = [make_grid(recon[:, :, t], nrow = int(np.sqrt(BATCH_SIZE))) for t in range(N_FRAMES_IN_TIME)]
+diff_grids = [igrid - rgrid for igrid, rgrid in zip(input_grids, recon_grids)]
+imageio.mimwrite('inputs.gif', [grid[0].float().cpu().numpy() for grid in input_grids], fps = 5)
+imageio.mimwrite('recons.gif', [grid[0].float().cpu().numpy() for grid in recon_grids], fps = 5)
+imageio.mimwrite('diffs.gif', [grid[0].float().cpu().numpy() for grid in diff_grids], fps = 5)
