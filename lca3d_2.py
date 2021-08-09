766d7f1d26da328f8ea75c0cba0065db07451bb8
Michael Teti <mtetifau@gmail.com>
2021-08-09 10:00:16 -0400

    move these to top

--- lca3d_2.py
+++ lca3d_2.py
@@ -26,6 +26,11 @@ class LCA3DConv(LCAConvBase):
     def __init__(self, kh=7, kw=7, kt=3, stride_h=1, stride_w=1, stride_t=1,
                  **kwargs):
         super(LCA3DConv, self).__init__(**kwargs)
+
+        assert (kh%2 != 0 and kw%2 != 0) or (kh%2 == 0 and kw%2 == 0), (
+                'kh and kw should either both be even or both be odd numbers')
+        assert stride_h == 1 or stride_h%2 == 0
+        assert stride_w == 1 or stride_w%2 == 0
         
         self.kh = kh
         self.kt = kt
@@ -33,12 +38,6 @@ class LCA3DConv(LCAConvBase):
         self.stride_h = stride_h 
         self.stride_t = stride_t 
         self.stride_w = stride_w
-
-        assert ((self.kh%2 != 0 and self.kw%2 != 0) or 
-                (self.kh%2 == 0 and self.kw%2 == 0)), (
-                'kh and kw should either both be even or both be odd numbers')
-        assert self.stride_h == 1 or self.stride_h%2 == 0
-        assert self.stride_w == 1 or self.stride_w%2 == 0
 
         self.create_weight_tensor()
         self.compute_padding_dims()
