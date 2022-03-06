a29dca780e5def169cdaf27995d4f9104fc15f52
Michael Teti <mtetifau@gmail.com>
2022-03-06 12:36:10 -0500

    testing shapes too

--- test/test_preproc.py
+++ test/test_preproc.py
@@ -15,6 +15,7 @@ class TestPreprocessing(unittest.TestCase):
     def test_standardize_inputs_3D_tensor(self):
         inputs = torch.rand(1, 3, 100) * 100
         outputs = standardize_inputs(inputs)
+        self.assertEqual(outputs.shape, inputs.shape)
         for chann in range(3):
             assert_close(outputs[:, chann].mean().item(), 0.0)
             assert_close(outputs[:, chann].std().item(), 1.0)
@@ -22,6 +23,7 @@ class TestPreprocessing(unittest.TestCase):
     def test_standardize_inputs_4D_tensor(self):
         inputs = torch.rand(1, 3, 100, 100) * 100
         outputs = standardize_inputs(inputs)
+        self.assertEqual(outputs.shape, inputs.shape)
         for chann in range(3):
             assert_close(outputs[:, chann].mean().item(), 0.0)
             assert_close(outputs[:, chann].std().item(), 1.0)
@@ -29,6 +31,7 @@ class TestPreprocessing(unittest.TestCase):
     def test_standardize_inputs_5D_tensor(self):
         inputs = torch.rand(1, 3, 4, 100, 100) * 100
         outputs = standardize_inputs(inputs)
+        self.assertEqual(outputs.shape, inputs.shape)
         for chann in range(3):
             for depth in range(4):
                 assert_close(outputs[:, chann, depth].mean().item(), 0.0)
