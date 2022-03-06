b39637933870925d7227a365480dd7f0ac1732c5
Michael Teti <mtetifau@gmail.com>
2022-03-06 10:58:02 -0500

    was testing 5D tensor incorrectly

--- test/test_preproc.py
+++ test/test_preproc.py
@@ -13,33 +13,31 @@ class TestPreprocessing(unittest.TestCase):
             standardize_inputs(torch.zeros(1, 2, 3, 4, 5, 6))
 
     def test_standardize_inputs_3D_tensor(self):
-        inputs = torch.rand(1, 3, 100) * 10
+        inputs = torch.rand(1, 3, 100) * 100
         outputs = standardize_inputs(inputs)
         for chann in range(3):
             assert_close(outputs[:, chann].mean().item(), 0.0)
             assert_close(outputs[:, chann].std().item(), 1.0)
 
     def test_standardize_inputs_4D_tensor(self):
-        inputs = torch.rand(1, 3, 100, 100) * 10
+        inputs = torch.rand(1, 3, 100, 100) * 100
         outputs = standardize_inputs(inputs)
         for chann in range(3):
             assert_close(outputs[:, chann].mean().item(), 0.0)
             assert_close(outputs[:, chann].std().item(), 1.0)
 
     def test_standardize_inputs_5D_tensor(self):
-        inputs = torch.rand(1, 3, 100, 100, 100) * 10
+        inputs = torch.rand(1, 3, 4, 100, 100) * 100
         outputs = standardize_inputs(inputs)
         for chann in range(3):
-            assert_close(outputs[:, chann].mean().item(), 0.0, rtol=5e-4,
-                         atol=5e-5)
-            assert_close(outputs[:, chann].std().item(), 1.0, rtol=5e-4,
-                         atol=5e-5)
+            for depth in range(4):
+                assert_close(outputs[:, chann, depth].mean().item(), 0.0)
+                assert_close(outputs[:, chann, depth].std().item(), 1.0)
 
     def test_standardize_inputs_already_standardized(self):
-        inputs_randn = torch.randn(1, 3, 100, 100, 100)
-        inputs_randn = standardize_inputs(inputs_randn)
-        outputs = standardize_inputs(inputs_randn)
-        assert_close(inputs_randn, outputs, rtol=1e-4, atol=1e-5)
+        inputs = standardize_inputs(torch.rand(1, 3, 10000))
+        outputs = standardize_inputs(inputs)
+        assert_close(inputs, outputs)
 
 
 if __name__ == '__main__':
