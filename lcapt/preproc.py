d0ea1d0d036e1265e5400ccda5b8d11a84fc239e
Michael Teti <mtetifau@gmail.com>
2022-07-08 14:00:13 -0600

    separated the mean subtraction and std division into two functions

--- lcapt/preproc.py
+++ lcapt/preproc.py
@@ -4,14 +4,25 @@ import torch
 Tensor = torch.Tensor
 
 
-def standardize_inputs(batch: Tensor, eps: float = 1e-8) -> Tensor:
-    """Standardize each sample in x"""
+def zero_mean(batch: Tensor) -> Tensor:
     if len(batch.shape) == 3:
         dims = -1
     elif len(batch.shape) in [4, 5]:
         dims = (-2, -1)
     else:
         raise NotImplementedError
+    
     mean = batch.mean(dim=dims, keepdim=True)
+    return batch - mean
+
+
+def contrast_norm(batch: Tensor, eps: float = 1e-8) -> Tensor:
+    if len(batch.shape) == 3:
+        dims = -1
+    elif len(batch.shape) in [4, 5]:
+        dims = (-2, -1)
+    else:
+        raise NotImplementedError
+
     std = batch.std(dim=dims, keepdim=True)
-    return (batch - mean) / (std + eps)
+    return batch / (std + eps)
