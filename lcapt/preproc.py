240ea2c92e6aed3d103ef2445cf22d43bbbf1615
Michael Teti <mtetifau@gmail.com>
2022-07-08 16:23:14 -0600

    more general this way

--- lcapt/preproc.py
+++ lcapt/preproc.py
@@ -5,28 +5,12 @@ Tensor = torch.Tensor
 
 
 def zero_mean(batch: Tensor) -> Tensor:
-    if len(batch.shape) == 3:
-        dims = (-2, -1)
-    elif len(batch.shape) == 4:
-        dims = (-3, -2, -1)
-    elif len(batch.shape) == 5:
-        dims = (-4, -3, -2, -1)
-    else:
-        raise NotImplementedError
-
+    dims = tuple(range((len(batch.shape) - 1) * -1, 0))
     mean = batch.mean(dim=dims, keepdim=True)
     return batch - mean
 
 
 def contrast_norm(batch: Tensor, eps: float = 1e-8) -> Tensor:
-    if len(batch.shape) == 3:
-        dims = (-2, -1)
-    elif len(batch.shape) == 4:
-        dims = (-3, -2, -1)
-    elif len(batch.shape) == 5:
-        dims = (-4, -3, -2, -1)
-    else:
-        raise NotImplementedError
-
+    dims = tuple(range((len(batch.shape) - 1) * -1, 0))
     std = batch.std(dim=dims, keepdim=True)
     return batch / (std + eps)
