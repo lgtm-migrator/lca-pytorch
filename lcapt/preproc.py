86ca0ec7b66534e5ce21b77ff382d5d2fa253b45
Michael Teti <mtetifau@gmail.com>
2022-07-09 12:09:07 -0600

    same thing but just way more readable

--- lcapt/preproc.py
+++ lcapt/preproc.py
@@ -5,12 +5,14 @@ Tensor = torch.Tensor
 
 
 def make_zero_mean(batch: Tensor) -> Tensor:
-    dims = tuple(range((len(batch.shape) - 1) * -1, 0))
+    """Make each sample in a batch have zero mean"""
+    dims = tuple(range(1, len(batch.shape)))
     mean = batch.mean(dim=dims, keepdim=True)
     return batch - mean
 
 
 def make_unit_var(batch: Tensor, eps: float = 1e-8) -> Tensor:
-    dims = tuple(range((len(batch.shape) - 1) * -1, 0))
+    """Make each sample in a batch have unit variance"""
+    dims = tuple(range(1, len(batch.shape)))
     std = batch.std(dim=dims, keepdim=True)
     return batch / (std + eps)
